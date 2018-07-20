dde3606240c3b86be496ae452fcbb9cf034ed90b
Keisuke Taniguchi <rememberthatk@icloud.com>
2018-07-20 07:27:43 +0000

    add flash helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def display_flash
+    flash.each do |type, msg|
+      type = if type == 'notice'
+               'success'
+             elsif type == 'alert'
+               'danger'
+             end
+      concat content_tag(:div, msg, class: "alert alert-#{type}")
+    end
+    nil
+  end
 end
