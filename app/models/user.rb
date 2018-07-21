48daeb8ebdc39aa241761c85bbe5f8e71ce4ea41
Keisuke Taniguchi <rememberthatk@icloud.com>
2018-07-21 06:05:27 +0000

    パスワードなしでもサインアップできるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :timeoutable, :omniauthable
+
+  def password_required?
+    super if confirmed?
+  end
 end
