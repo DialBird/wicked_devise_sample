35de9b406ab78580ac7308b693e39c2a61b97c5b
Keisuke Taniguchi <rememberthatk@icloud.com>
2018-07-22 00:33:19 +0000

    User::ATTRIBUTESを追加する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :timeoutable, :omniauthable
 
+  ATTRIBUTES = %i[
+    display_name sex_id last_name first_name usage_type_id corp_name
+    zip prefecture_id address_1 address_2 phone
+  ].freeze
+
   def password_required?
     super if confirmed?
   end
